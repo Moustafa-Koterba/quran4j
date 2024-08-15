7077d6cf0bbbf80fa7baefef06a4775e48993afa
Moustafa <Moustafa.Koterba@gmail.com>
2024-08-15 16:27:09 +0200

    setup liquibase + admin endpoint to update surah title and origin step 2

--- api/src/main/java/quran4j/api/domain/SurahDomain.java
+++ api/src/main/java/quran4j/api/domain/SurahDomain.java
@@ -3,12 +3,13 @@ package quran4j.api.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import quran4j.api.model.Origin;
 
 import java.util.List;
 import java.util.Locale;
 
 @Entity
-@Table(name = "surah", uniqueConstraints = { @UniqueConstraint(columnNames = { "index", "locale" }) })
+@Table(name = "surah")
 @Getter
 @Setter
 public class SurahDomain {
@@ -18,6 +19,8 @@ public class SurahDomain {
     private int index;
     private String title;
     private Locale locale;
+    @Enumerated(EnumType.STRING)
+    private Origin origin;
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "surah", cascade = CascadeType.PERSIST)
     private List<AyatDomain> ayats;
 }
