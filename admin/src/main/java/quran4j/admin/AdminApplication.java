c1dff7472e8f069b5bb94de883bd158671e8a51c
Moustafa <Moustafa.Koterba@gmail.com>
2024-09-07 19:30:40 +0200

    change 6

--- admin/src/main/java/quran4j/admin/AdminApplication.java
+++ admin/src/main/java/quran4j/admin/AdminApplication.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "quran4j.library")
 public class AdminApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello world 5");
+		System.out.println("Hello world 6");
 		SpringApplication.run(AdminApplication.class, args);
 	}
 }
