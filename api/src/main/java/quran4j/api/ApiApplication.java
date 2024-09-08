3b592906779567583cb140ab81ebd137e00c6193
Moustafa <Moustafa.Koterba@gmail.com>
2024-09-08 18:49:51 +0200

    change api

--- api/src/main/java/quran4j/api/ApiApplication.java
+++ api/src/main/java/quran4j/api/ApiApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
